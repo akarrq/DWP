08aa610b9e754ef8aa2ed1e84ef4fd0a1335c3ec
Błażej Plichta <b.plichta96@gmail.com>
2023-02-23 19:46:08 +0100

    added car script on events site

--- Car.js
+++ Car.js
@@ -5,37 +5,68 @@ class Car {
     this.confirmBtn = null;
     this.odometer = null;
     this.confirmMessage = null;
+
+    this.isEventsSite = null;
+  }
+  checkWhichSite(site) {
+    console.log(site);
+    if (site === "events") {
+      this.isEventsSite = true;
+    } else {
+      this.isEventsSite = false;
+    }
+    this.addCarClickEvent();
   }
   addCarClickEvent() {
     this.cars = document.querySelectorAll('[data-id="car"]');
     for (const car of this.cars) {
       car.onclick = this.createCarCard.bind(this);
       car.style = "cursor:pointer";
+      console.log("dodano klik");
     }
   }
   createCarCard(car) {
+    console.log("!! kliknęło w samochód");
     if (car.target.closest('[data-id="car"]')) this.car = car.currentTarget;
     this.removeCarClickEvent();
     this.car.classList.add("carEdit");
     this.switchCardVisible();
     this.switchDots();
-
+    if (!this.isEventsSite) {
+      console.log("dodano licznik");
+      this.createConfirmBtn();
+      this.createOdometer();
+    } else {
+      console.log("dodano wiadomość");
+      this.createConfirmBtn();
+      this.showNothingHereMessage();
+    }
+  }
+  createOdometer() {
     this.odometer = document.createElement("input");
     this.odometer.type = "number";
     this.odometer.inputMode = "numeric";
     this.odometer.placeholder = "Wprowadź przebieg";
     this.car.appendChild(this.odometer).classList.add("carEdit__odometer");
-
+  }
+  createConfirmBtn() {
     this.confirmBtn = document.createElement("h4");
     this.confirmBtn.textContent = "Gotowe";
     this.confirmBtn.classList.add("carEdit__confirmBtn");
     this.car.prepend(this.confirmBtn);
-    this.confirmBtn.onclick = this.showConfirmMessage.bind(this);
+    console.log("dodano przycisk");
+    if (!this.isEventsSite) {
+      this.confirmBtn.onclick = this.showConfirmMessage.bind(this);
+      console.log("pokaże wprowadzono zmiany");
+    } else {
+      this.confirmBtn.onclick = this.closeCarCard.bind(this);
+    }
   }
   removeCarClickEvent() {
     for (const car of this.cars) {
       car.onclick = "";
       car.style = "cursor:default";
+      console.log("usunięto klik");
     }
   }
   showConfirmMessage() {
@@ -45,16 +76,29 @@ class Car {
     this.odometer.replaceWith(this.confirmMessage);
     setTimeout(this.closeCarCard.bind(this), 0.6 * 1000);
   }
+  showNothingHereMessage() {
+    this.confirmMessage = document.createElement("p");
+    this.confirmMessage.classList.add("carEdit__confirmMessage");
+    this.confirmMessage.textContent = "Nic tu nie ma. Uff!";
+    this.car.append(this.confirmMessage);
+  }
   closeCarCard() {
     this.confirmBtn.remove();
-    this.odometer.remove();
+    console.log("usunięto przycisk");
+    if (!this.isEventsSite) {
+      this.odometer.remove();
+      console.log("usunięto licznik");
+    }
     this.confirmMessage.remove();
     this.switchCardVisible();
+    console.log("usunięto wiadomość");
     this.switchDots();
     this.car.classList.remove("carEdit");
+    this.confirmBtn.onclick = "";
     this.addCarClickEvent();
   }
   switchCardVisible() {
+    console.log("zmieniono widoczność");
     for (const car of this.cars) {
       if (!car.classList.contains("carEdit")) {
         car.classList.toggle("carEdit__inactive");
@@ -62,6 +106,7 @@ class Car {
     }
   }
   switchDots() {
+    console.log("zmieniono kropki");
     const dots = document.querySelectorAll('[data-id="dot"]');
     for (const dot of dots) {
       dot.classList.toggle("board__dot");
@@ -69,6 +114,3 @@ class Car {
     }
   }
 }
-
-let initialCar = new Car();
-initialCar.addCarClickEvent();
